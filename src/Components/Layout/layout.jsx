c4a9ebea6bff391408785d050942ba636ec3b14a
dksgmlwls <dkstodl@naver.com>
2025-06-04 23:41:42 +0900

    처음 커밋: Home.jsx로 메인 설정 및 header 추가

---
+++ src/Components/Layout/layout.jsx
@@ -0,0 +1,11 @@
+import Header from "./hearder/hearder";
+
+const Layout = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default Layout
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Layout from "../Components/Layout/layout";
+
+const HomePage = () => {
+    return (
+        <Layout>
+
+        </Layout>
+    )
+}
+
+export default HomePage
